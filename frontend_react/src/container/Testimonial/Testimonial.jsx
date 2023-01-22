81a6e2d2de421338c8bfcdbf90ddee2bb15ecff2
Kelli Libin <km.putnam2@gmail.com>
2023-01-22 12:01:40 -0700

    added testimonials and styled

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 
 //libraries
 import { motion } from "framer-motion";
-import {HiChevronLeft, HiChevronRight} from 'react-icons/hi'
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 //components and pages
 import { AppWrap, MotionWrap } from "../../wrapper";
@@ -11,33 +11,78 @@ import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 
 //styles
-import './Testimonial.scss'
+import "./Testimonial.scss";
 
 const Testimonial = () => {
   const [brands, setBrands] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const query = '*[type == "testimonials"]';
-  const brandsQuery = '*[type == "brands"]';
+  //handles moving from one testimonial to another
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  //set up queries and connect to sanity
+  const query = '*[_type == "testimonials"]';
+  const brandsQuery = '*[_type == "brands"]';
 
   useEffect(() => {
     client.fetch(query).then((data) => {
       setTestimonials(data);
-    })
+    });
 
     client.fetch(brandsQuery).then((data) => {
       setBrands(data);
-    })
+    });
   }, []);
-  return (
-    <div>Testimonial</div>
-  )
-}
 
+  //will show one testimonial at a time, based on index
+  const test = testimonials[currentIndex];
+  return (
+    <>
+      {testimonials.length && (
+        <>
+          <div className="app__testimonial-item app__flex">
+            <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+            <div className="app__testimonial-content">
+              <p className="p-text">{testimonials[currentIndex].feedback}</p>
+              <div>
+                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+              </div>
+            </div>
+          </div>
+
+          <div className="app__testimonial-btns app__flex">
+            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+
+            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+        </>
+      )}
+
+      <div className="app__testimonial-brands app__flex">
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand._id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default AppWrap(
-  MotionWrap(Testimonial, "app__testimonials"),
-  "testimonial",
-  "app__primarybg"
+  MotionWrap(Testimonial, 'app__testimonial'),
+  'testimonial',
+  'app__primarybg',
 );
