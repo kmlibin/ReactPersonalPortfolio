8e1f3d687f4ab27104a8170b40d84c60fa6082d5
Kelli Libin <km.libin2@gmail.com>
2023-01-21 17:35:05 -0700

    created, set up queries and imports

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+
+//libraries
+import { motion } from "framer-motion";
+import {HiChevronLeft, HiChevronRight} from 'react-icons/hi'
+
+//components and pages
+import { AppWrap, MotionWrap } from "../../wrapper";
+
+//sanity
+import { urlFor, client } from "../../client";
 
 //styles
 import './Testimonial.scss'
 
-export default function Testimonial() {
+const Testimonial = () => {
+  const [brands, setBrands] = useState([]);
+  const [testimonials, setTestimonials] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const query = '*[type == "testimonials"]';
+  const brandsQuery = '*[type == "brands"]';
+
+  useEffect(() => {
+    client.fetch(query).then((data) => {
+      setTestimonials(data);
+    })
+
+    client.fetch(brandsQuery).then((data) => {
+      setBrands(data);
+    })
+  }, []);
   return (
     <div>Testimonial</div>
   )
 }
+
+
+export default AppWrap(
+  MotionWrap(Testimonial, "app__testimonials"),
+  "testimonial",
+  "app__primarybg"
+);
