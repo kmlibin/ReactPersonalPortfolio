cdc033514f5a2f4e76fc56720441a5ecbe5ff29e
Kelli Libin <km.putnam2@gmail.com>
2023-01-31 12:44:20 -0700

    put interfaces in same place, exported

---
+++ frontend_react/src/models/model.tsx
@@ -0,0 +1,54 @@
+interface IAsset {
+  asset: {
+    _ref: string;
+    _type?: string;
+  };
+}
+
+export interface ISkills {
+  bgColor: string;
+  icon: IAsset;
+  name: string;
+}
+
+//for skills.tsx ...since the code later maps within a map, had to split into two interfaces.
+export interface IWork {
+  company: string;
+  desc: string;
+  name: string;
+}
+
+export interface IExperience {
+  works: IWork[];
+  year: string;
+}
+
+//for testimonial.tsx
+export interface ITestimonial {
+  company: string;
+  feedback: string;
+  imgurl: IAsset;
+  name: string;
+}
+
+export interface IBrand {
+  imgUrl: IAsset;
+  name: string;
+  _id: string;
+}
+
+//for work.tsx
+export interface IAnimateCard {
+  y: number;
+  opacity: number;
+}
+
+export interface IWorks {
+  codeLink: string;
+  description: string;
+  imgUrl: IAsset;
+  projectLink: string;
+  tags: [string[] | string];
+  title: string;
+  name?: string;
+}
