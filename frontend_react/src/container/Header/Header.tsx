94379ed6f4c85d68e2d562649cc54f23bc837275
Kelli Libin <km.putnam2@gmail.com>
2023-02-02 15:58:11 -0700

    updated emoji and p text

--- frontend_react/src/container/Header/Header.tsx
+++ frontend_react/src/container/Header/Header.tsx
@@ -33,15 +33,16 @@ const Header: React.FC = () => {
       >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            <span>👋</span>
+            <span>👋🏻</span>
             <div style={{ marginLeft: 20 }}>
               <p className="p-text">Hello, I am </p>
-              <h1 className="head-text">Micael</h1>
+              <h1 className="head-text">Kelli</h1>
             </div>
           </div>
           <div className="tag-cmp app__flex">
             <p className="p-text">Web Developer</p>
             <p className="p-text">Freelancer</p>
+            <p className="p-text">Cat-Herder</p>
           </div>
         </div>
       </motion.div>
@@ -66,7 +67,7 @@ const Header: React.FC = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((item, index) => (
+        {[images.typescript, images.react, images.sass].map((item, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={item} alt="circle" />
           </div>
