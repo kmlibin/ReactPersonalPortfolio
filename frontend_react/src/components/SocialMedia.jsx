7a5f04dff96eeb8277fb71d56f9ba18eb69b657a
Kelli Libin <km.putnam2@gmail.com>
2023-02-02 15:53:49 -0700

    updated icons, added links

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import {BsTwitter, BsInstagram} from 'react-icons/bs';
-import {FaFacebookF} from 'react-icons/fa';
+import {FiGithub} from 'react-icons/fi';
+import {FaEtsy, FaLinkedinIn} from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <BsTwitter />
+            <a href="https://github.com/kmlibin" target="_blank"><FiGithub /></a>
         </div>
         <div>
-            <FaFacebookF />
+            <a href="https://linkedin.com/in/kelli-libin-b5ba78151/" target="_blank"><FaLinkedinIn /></a>
         </div>
         <div>
-            <BsInstagram />
+            <a href="https://www.etsy.com/shop/CreationsKMP" target="_blank"><FaEtsy /></a>
         </div>
     </div>
   )
