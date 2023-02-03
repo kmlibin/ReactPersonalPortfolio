6fcdc5cfc4294cb320c4ae4ccab756af7d4531c9
Kelli Libin <km.putnam2@gmail.com>
2023-02-03 15:21:05 -0700

    added color to &

--- frontend_react/src/container/Skills/Skills.tsx
+++ frontend_react/src/container/Skills/Skills.tsx
@@ -37,7 +37,7 @@ const Skills: React.FC = () => {
 
   return (
     <>
-      <h2 className="head-text">Skills & Experiences</h2>
+      <h2 className="head-text">Skills <span>&</span> Experiences</h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
