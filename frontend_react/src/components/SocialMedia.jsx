7c36268d6679b8fc74d363d753e5aabab1b363d9
Kelli Libin <km.putnam2@gmail.com>
2023-02-03 10:08:52 -0700

    added classnames for styling

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -6,13 +6,13 @@ const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <a href="https://github.com/kmlibin" target="_blank"><FiGithub /></a>
+            <a className="app__social-icons" href="https://github.com/kmlibin" target="_blank"><FiGithub /></a>
         </div>
         <div>
-            <a href="https://linkedin.com/in/kelli-libin-b5ba78151/" target="_blank"><FaLinkedinIn /></a>
+            <a className="app__social-icons" href="https://linkedin.com/in/kelli-libin-b5ba78151/" target="_blank"><FaLinkedinIn /></a>
         </div>
         <div>
-            <a href="https://www.etsy.com/shop/CreationsKMP" target="_blank"><FaEtsy /></a>
+            <a className="app__social-icons" href="https://www.etsy.com/shop/CreationsKMP" target="_blank"><FaEtsy /></a>
         </div>
     </div>
   )
