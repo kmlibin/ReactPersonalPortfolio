b96481a52deaa42bf92129dd7cfb50108bd9db9e
Kelli Libin <km.putnam2@gmail.com>
2023-02-08 15:59:46 -0700

    delete

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,63 +1,33 @@
-import email from '../assets/email.png';
-import mobile from '../assets/mobile.png';
-
-
-import css from '../assets/css.png';
-
-import git from '../assets/git.png';
-import graphql from '../assets/graphql.png';
-import html from '../assets/html.png';
-import javascript from '../assets/javascript.png';
-
-import node from '../assets/node.png';
-
-import react from '../assets/react.png';
-import redux from '../assets/redux.png';
-import sass from '../assets/sass.png';
-import typescript from '../assets/typescript.png';
-
-
-
-import profile from '../assets/kmlibincat.png';
-import circle from '../assets/circle.svg';
-import logo from '../assets/klogo.png';
-
-
-import bolt from '../assets/bolt.png';
-
-import skype from '../assets/skype.png';
-import spotify from '../assets/spotify.png';
-import background from '../assets/background.jpg'
-
+import email from "../assets/email.png";
+import mobile from "../assets/mobile.png";
+import css from "../assets/css.png";
+import git from "../assets/git.png";
+import html from "../assets/html.png";
+import javascript from "../assets/javascript.png";
+import node from "../assets/node.png";
+import react from "../assets/react.png";
+import redux from "../assets/redux.png";
+import sass from "../assets/sass.png";
+import typescript from "../assets/typescript.png";
+import profile from "../assets/kmlibincat.png";
+import circle from "../assets/circle.svg";
+import logo from "../assets/klogo.png";
+import background from "../assets/background.jpg";
 
 export default {
   email,
   mobile,
-  
   css,
- 
   git,
-  graphql,
   html,
   javascript,
- 
   node,
-
   react,
   redux,
   sass,
   typescript,
-
   profile,
   circle,
   logo,
- 
-  bolt,
-
-  skype,
-  spotify,
-
-
   background,
- 
 };
