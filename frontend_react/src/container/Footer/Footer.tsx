91cc3bf0cbe899d00dd16e9943c5339f2798f4c0
Kelli Libin <km.putnam2@gmail.com>
2023-02-10 15:23:56 -0700

    deleted old styles, moved h2

--- frontend_react/src/container/Footer/Footer.tsx
+++ frontend_react/src/container/Footer/Footer.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 
-//import images
-import { images } from "../../constants";
-
 //components, pages, wrappers
 import { AppWrap, MotionWrap } from "../../wrapper";
 
@@ -55,10 +52,10 @@ const Footer: React.FC = () => {
 
   return (
     <>
-      <h2 className="head-text">Let's Get Acquainted!</h2>
-
+      
       {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
+          <h2 className="head-text">Let's Get Acquainted!</h2>
           <div className="app__flex">
             <input
               className="p-text"
