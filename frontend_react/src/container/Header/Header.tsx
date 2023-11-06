206cfaff505d8e0d3b040d8cba8c80d8f5d17cdf
Kelli Libin <km.putnam2@gmail.com>
2023-11-06 11:43:29 -0700

    sass to node logo

--- frontend_react/src/container/Header/Header.tsx
+++ frontend_react/src/container/Header/Header.tsx
@@ -67,7 +67,7 @@ const Header: React.FC = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.sass, images.react, images.typescript].map((item, index) => (
+        {[images.nodelogo, images.react, images.typescript].map((item, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={item} alt="circle" />
           </div>
