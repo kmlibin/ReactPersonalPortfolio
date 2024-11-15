34fb2e635b29aaf36a4b61a8dd957b5600185119
Kelli Libin <km.putnam2@gmail.com>
2024-11-15 15:20:35 -0700

    fade in images

--- frontend_react/src/container/Testimonial/Testimonial.tsx
+++ frontend_react/src/container/Testimonial/Testimonial.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 //libraries
 import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 //interfaces
 import { ITestimonial } from "../../models/model";
@@ -18,10 +19,12 @@ import "./Testimonial.scss";
 const Testimonial: React.FC = () => {
   const [testimonials, setTestimonials] = useState<ITestimonial[]>([]);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [fadeKey, setFadeKey] = useState(0);
 
-  //handles moving from one testimonial to another
+  //handles moving from one testimonial to another, also controls the fade of images
   const handleClick = (index: number) => {
     setCurrentIndex(index);
+    setFadeKey((prevKey) => prevKey + 1);
   };
 
   //set up queries and connect to sanity
@@ -40,7 +43,15 @@ const Testimonial: React.FC = () => {
       {testimonials.length && (
         <>
           <div className="app__testimonial-item app__flex">
-            <img src={urlFor(test.imgurl)} alt={test.name} />
+            <motion.img
+              src={urlFor(test.imgurl)}
+             
+              key={fadeKey}
+              alt={test.name}
+              initial={{ opacity: 0, scale: 0.8 }} // Start slightly smaller and invisible
+              animate={{ opacity: 1, scale: 1 }} // End fully visible and normal size
+              transition={{ duration: 1 }} // Customize duration for the image
+            />
             <div className="app__testimonial-content">
               <p className="p-text">{test.feedback}</p>
               <div>
