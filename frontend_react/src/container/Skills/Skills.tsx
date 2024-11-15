f664f2b1651097f8a5a505f2832390dae5947641
Kelli Libin <km.putnam2@gmail.com>
2024-11-15 15:12:37 -0700

    reformatted skills display

--- frontend_react/src/container/Skills/Skills.tsx
+++ frontend_react/src/container/Skills/Skills.tsx
@@ -16,28 +16,48 @@ import { urlFor, client } from "../../client";
 //styles
 import "./Skills.scss";
 import "react-tooltip/dist/react-tooltip.css";
+interface WorkExperience {
+  _id: string;
+  company: string;
+  desc: string;
+  name: string;
+  year1: string;
+  year2: string;
+  _type: string;
+  _createdAt: string;
+  _updatedAt: string;
+  _rev: string;
+  key:string;
+}
 
 const Skills: React.FC = () => {
   const [skills, setSkills] = useState<ISkills[]>([]);
-  const [experience, setExperience] = useState<IExperience[]>([]);
+  const [experience, setExperience] = useState<WorkExperience[]>([]);
 
   //connect to sanity
   useEffect(() => {
-    const query = '*[_type == "experiences"]';
+    const query = '*[_type == "workExperience"]';
+
+    client.fetch<WorkExperience[]>(query).then((data) => {
+      const sortedData = data.sort((a, b) => {
+        // Handle comparison based on year1 or year2
+        const yearA = parseInt(a.year1, 10) || 0; // Default to 0 if year1 is invalid
+        const yearB = parseInt(b.year1, 10) || 0;
+        return yearB - yearA; // Descending order
+      });
+      setExperience(sortedData);
+    });
+
     const skillsQuery = '*[_type == "skills"]';
-
-    client.fetch(query).then((data) => {
-      setExperience(data);
-    });
-
-    client.fetch(skillsQuery).then((data) => {
-      setSkills(data);
-    });
+    client.fetch(skillsQuery).then((data) => setSkills(data));
   }, []);
 
+  console.log(experience);
   return (
     <>
-      <h2 className="head-text">Skills <span>&</span> Experiences</h2>
+      <h2 className="head-text">
+        Skills <span>&</span> Experiences
+      </h2>
 
       <div className="app__skills-container">
         <motion.div className="app__skills-list">
@@ -59,34 +79,35 @@ const Skills: React.FC = () => {
           ))}
         </motion.div>
         <div className="app__skills-exp">
-          {experience.map((experience) => (
+          {experience.map((experience, index) => (
             <motion.div className="app__skills-exp-item" key={experience._id}>
               <div className="app__skills-exp-year">
-                <p className="bold-text">{experience.year}</p>
+                <p className="bold-text">{experience.year1}</p>{" "}
+                {experience.year2 && experience.year2 != "none" ? (
+                  <p>&nbsp;- {experience.year2}</p>
+                ) : null}
               </div>
               <motion.div className="app__skills-exp-works">
-                {experience.works.map((work) => (
-                  <>
-                    <motion.div
-                      whileInView={{ opacity: [0, 1] }}
-                      transition={{ duration: 0.5 }}
-                      className="app__skills-exp-work"
-                      data-tip
-                      data-for={work.name}
-                      key={work._key}
-                    >
-                      <h4 id={work.name}>{work.name}</h4>
-                      <p className="p-text">{work.company}</p>
-                    </motion.div>
-                    <Tooltip
-                      anchorId={work.name}
-                      data-tooltip-place="top"
-                      data-tooltip-variant="#fff"
-                      className="skills-tooltip"
-                      content={work.desc}
-                    />
-                  </>
-                ))}
+                <>
+                  <motion.div
+                    whileInView={{ opacity: [0, 1] }}
+                    transition={{ duration: 0.5 }}
+                    className="app__skills-exp-work"
+                    data-tip
+                    data-for={experience._id}
+                    key={experience.key}
+                  >
+                    <h4 id={experience._id}>{experience.name}</h4>
+                    <p className="p-text">{experience.company}</p>
+                  </motion.div>
+                  <Tooltip
+                    anchorId={experience._id}
+                    data-tooltip-place="top"
+                    data-tooltip-variant="#fff"
+                    className="skills-tooltip"
+                    content={experience.desc}
+                  />
+                </>
               </motion.div>
             </motion.div>
           ))}
